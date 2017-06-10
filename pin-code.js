54100bce022996f851e027286f9b397e48d287b3
Dekel Barzilay <dekelev@gmail.com>
2017-06-10 21:45:51 +0300

    * Added defaultCode & onChange props and made code & onSuccess props optional * Added numeric option to force input to be digits only * Added Backspace handler that will erase & focus previous pin when the current pin has no value (iOS only - untested) * Added maxLength=1 to each pin * Refactored code

--- pin-code.js
+++ pin-code.js
@@ -10,7 +10,7 @@ class CodePin extends Component {
 
     this.state = {
       error: '',
-      code: new Array(props.number).fill(''),
+      code: props.defaultCode ? props.defaultCode.split('') : new Array(props.number).fill(''),
       edit: 0
     };
 
@@ -29,43 +29,70 @@ class CodePin extends Component {
     this.textInputsRefs[id].focus();
   };
 
+  removeFocus = id => {
+    this.textInputsRefs[id].blur();
+  };
+
   isFocus = id => {
     let newCode = this.state.code.slice();
+    let codeChanged = false;
 
     for (let i = 0; i < newCode.length; i++)
       if (i >= id) newCode[i] = '';
+
+    if (this.state.code !== newCode)
+      codeChanged = true;
 
     this.setState({
       code: newCode,
       edit: id
     });
+
+    if (codeChanged)
+      this.props.onChange(newCode.join(''));
   };
 
-  handleEdit = (number, id) => {
+  handleEdit = (char, id) => {
     let newCode = this.state.code.slice();
-    newCode[id] = number;
+
+    if (this.props.numeric) {
+      const digit = char.match(/\d+/g);
+      if (!digit) {
+        newCode[id] = digit;
+        this.setState({ code: newCode });
+
+        return;
+      }
+    }
+
+    newCode[id] = char;
 
     // User filling the last pin ?
     if (id === this.props.number - 1) {
       // But it's different than code
-      if (this.props.code !== newCode.join('')) {
+      if (this.props.code && this.props.code !== newCode.join('')) {
         this.focus(0);
 
         this.setState({
           error: this.props.error,
           code: new Array(this.props.number).fill(''),
-          edit: 0
+          edit: 0,
         });
 
         return;
       } else {
+        this.removeFocus(id);
+
         this.setState({
           error: '',
           code: newCode,
-          edit: this.state.edit
         });
 
-        this.props.success();
+        if (this.props.onChange)
+          this.props.onChange(newCode.join(''));
+
+        if (this.props.onSuccess)
+          this.props.onSuccess();
       }
 
       return;
@@ -77,16 +104,38 @@ class CodePin extends Component {
       return {
         error: '',
         code: newCode,
-        edit: prevState.edit + 1
+        edit: prevState.edit + 1,
       };
     });
+
+    if (this.props.onChange)
+      this.props.onChange(newCode.join(''));
+  };
+
+  // TODO: test on iOS
+  handleKeyPress = (event, id) => {
+    const key = event.key;
+    let newCode = this.state.code.slice();
+
+    if (id && key === 'Backspace' && !this.state.code[id]) {
+      newCode[id - 1] = '';
+
+      this.focus(this.state.edit - 1);
+
+      this.setState(prevState => {
+        return {
+          error: '',
+          code: newCode,
+          edit: prevState.edit - 1,
+        };
+      });
+    }
   };
 
   render() {
     const {
       text,
       number,
-      success,
       pinStyle,
       textStyle,
       errorStyle,
@@ -105,28 +154,33 @@ class CodePin extends Component {
           ref={ref => (this.textInputsRefs[id] = ref)}
           onChangeText={text => this.handleEdit(text, id)}
           onFocus={() => this.isFocus(id)}
+          onKeyPress={event => this.handleKeyPress(event, id)}
           value={this.state.code[id] ? this.state.code[id].toString() : ''}
           style={[codePinStyles.pin, pinStyle]}
           returnKeyType={'done'}
           autoCapitalize={'sentences'}
           autoCorrect={false}
+          keyboardType={this.props.numeric ? 'numeric' : 'default'}
+          maxLength={1}
           {...props}
         />
       );
     }
 
-    const error = this.state.error
+    const error = this.props.code && this.state.error
       ? <Text style={[codePinStyles.error, errorStyle]}>
-          {this.state.error}
-        </Text>
+        {this.state.error}
+      </Text>
       : null;
 
     return (
       <View style={[codePinStyles.container, containerStyle]}>
 
+        { this.props.code &&
         <Text style={[codePinStyles.text, textStyle]}>
           {text}
         </Text>
+        }
 
         {error}
 
@@ -142,14 +196,17 @@ class CodePin extends Component {
 }
 
 CodePin.propTypes = {
-  code: PropTypes.string.isRequired,
-  success: PropTypes.func.isRequired,
+  onSuccess: PropTypes.func,
+  onChange: PropTypes.func,
+  code: PropTypes.string,
+  defaultCode: PropTypes.string,
   number: PropTypes.number,
   pinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
   containerPinStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
   containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
   textStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  errorStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
+  errorStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+  numeric: PropTypes.bool,
 };
 
 CodePin.defaultProps = {
@@ -160,7 +217,8 @@ CodePin.defaultProps = {
   containerPinStyle: {},
   containerStyle: {},
   textStyle: {},
-  errorStyle: {}
+  errorStyle: {},
+  numeric: false,
 };
 
 export default CodePin;
